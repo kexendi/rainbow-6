2fccd72b055df928a0aae0d91b0ac3d9dacc0fdf
Kácsándi Máté Zsolt <kacsandi.mate@gmail.com>
2025-12-27 19:27:38 +0100

    inital commit

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('rainbow-6');
+  protected readonly title = signal('frontend');
 }
--- src/app/app.routes.ts
+++
@@ -1,6 +0,0 @@
-import { Routes } from '@angular/router';
-import { Welcome } from './welcome/welcome';
-
-export const routes: Routes = [
-    {path: '', component: Welcome}
-];
